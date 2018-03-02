c1f6ac2cfe00ce7aaf0e098331d6e6ff8b0695da
Max Korsun <mkorsun@stuy.edu>
2018-03-01 22:19:36 -0500

    done

--- svg.js
+++ svg.js
@@ -2,88 +2,77 @@
 var pic = document.getElementById("svg_id");
 
 var clears = function(e) {
-    while (c.hasChildNodes()){
-	c.removeChild(c.firstChild)
+    while (pic.hasChildNodes()){
+	pic.removeChild(pic.firstChild)
     }
-    prevx = -1;
-    prevy = -1;
 }
 
 
 
-var cbutton = document.getElementByID("clear");
+var cbutton = document.getElementById("clear");
 cbutton.addEventListener("click",clears);
 
 
-var r = 0;
+var r = 1;
 var d = 1;
 
 var circle = function(cx,cy,rad){
-    clears();
-    var circ = document.createElementNS("http://www.w3.org/2000/svg", "circle")
-    circ.setAttribute("cx",cx+"");
+    var circ = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    circ.setAttribute("cx",cx+"" );
     circ.setAttribute("cy",cy+"");
-    circ.setAttribute("r",r+"");
+    circ.setAttribute("r",rad+"");
     circ.setAttribute("fill","black");
     circ.setAttribute("stroke","black");
     pic.appendChild(circ);
 }
 var expand = function(e){
+    clears();
     if(r>=250 || r<=0){
 	d=d*-1;
     }
-    r=r+direction;
-    circle(0,0,r)
+    r=r+d;
+    circle(250,250,r);
     
 }
 
 var intervalID;
 var keepexpand = function(e){
-    intervalID = setInterval(expand,50);
+    intervalID = setInterval(expand,10);
 }
-var expbutton=document.getElementByID("expand");
+var expbutton=document.getElementById("expand");
 expbutton.addEventListener("click",keepexpand);
 
 var zawarudo = function(e){
     clearInterval(intervalID);
+    r=1;
+    d=1;
 }
 
 var stop = document.getElementById("stop");
-stop.addEventListener("click",zawurdo);
+stop.addEventListener("click",zawarudo);
 
+var bx = 250;
+var by = 250;
+var xchange = 1;
+var ychange= 1;
+var bounce = function(){
+    if (bx>= 270 || bx<=1){
+	xchange= xchange*=-1;
+    }
+    if(by>= 499 || by<=1){
+	ychange=ychange*-1;
+    }
+}
 var dvd =function(e) {
-    var x = 250;
-    var y = 250;
-    var xchange = 1;
-    var ychange= 1;
-    var ball = function(){
-	clears();
-	ctx.beginPath();
-	ctx.arc(x,y,25, 0, 2*Math.PI);
-	ctx.stroke();
-    }
-    var bounce = function(){
-	if (x>= 270 || x<=1){
-	    xchange= xchange*=-1;
-	}
-	if(y>= 499 || y<=1){
-	    ychange=ychange*-1;
-	}
-    }
-    window.cancelAnimationFrame(requestID);
-    
-    var draw = function(){
-	x = x+ xchange;
-	y=y+ychange;
-	bounce();
-	ball();
-	requestID = window.requestAnimationFrame(draw);
-	console.log(requestID);
-    }
-    draw();
+    clears();
+    bx = bx+ xchange;
+    by=by+ychange;
+    bounce();
+    circle(bx,by,25);
 }
 
 var play = function(e) {
-    window.requestAnimationFrame(dvd);
+    intervalID = setInterval(dvd,10);
 }
 office.addEventListener("click", play);	
+
